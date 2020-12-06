0474c66cc9f64b9821186c8771345f26c4af1f6b
syedoan <syedoan@fibermountain.com>
2020-12-06 11:44:20 +0000

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ouyeeeeeeeeeee!!!!!!. What the hell are you even doing here? Shuuu, go away.
         </p>
         <a
           className="App-link"
